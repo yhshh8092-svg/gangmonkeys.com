153c2e4a2e69c8417298619821aa2d10f5d2cb59
yhshh8092-svg <yhshh8092@gmail.com>
2025-10-21 19:06:33 -0400

    Create mock-data.js

---
+++ js/mock-data.js
@@ -0,0 +1,7 @@
+window.MOCK = {
+  albums:[
+    {name:"Chill Hits", artist:"Various", img:"https://i.scdn.co/image/ab67706f00000002b7d6b1a2f5e5c0d1c0e3f5a1", id:"1"},
+    {name:"Indie Rock", artist:"Indie", img:"https://i.scdn.co/image/ab67706f00000002c0c0c0c0c0c0c0c0c0c0c0c", id:"2"},
+    {name:"Deep Focus", artist:"Focus", img:"https://i.scdn.co/image/ab67706f00000002d1d1d1d1d1d1d1d1d1d1d1d", id:"3"}
+  ]
+};
